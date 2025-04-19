29203206f6feae0a0e1f16218dca4246055663c1
Diondre-Dubose <diondredubose123@gmail.com>
2025-04-19 21:41:56 +0000

    second commit

---
+++ Chapter1/car_prices.py
@@ -0,0 +1,6 @@
+# Building a linear regression model for predicting the car prices based on mileage and year using real world data
+
+import kagglehub 
+# Download latest version
+path = kagglehub.dataset_download("austinreese/craigslist-carstrucks-data")
+print("Path to dataset files:", path)
